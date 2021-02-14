2d01bf6d97fb34942b2eb3d43ff69183bb009ff5
YaLameR <reaf_ivan@mail.ru>
2021-02-14 13:32:32 +0300

    Отображение карты

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+import pygame
+import requests
+
+def map_resopnse():
+    map_request = "https://static-maps.yandex.ru/1.x/?l=map&ll=37.677751,55.757718&z=2"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
+
+map_file = map_resopnse()
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
